ea225a6e1a4311649412763c6b61303db96f7f0c
Ober11 <126175091+Ober11@users.noreply.github.com>
2024-02-23 19:00:57 +0100

    b

--- program.py
+++ program.py
@@ -24,16 +24,17 @@ def el(speed, megt, target=g.angle, multiplier = 0.8, stop = True):
     while remfok > 0:
         megtfok = (ml.position+mr.position)/2
         remfok = megt - megtfok
-        if megtfok < 51:
-            speed = max(megtfok * (originalspeed*0.02), 5)
+        if megtfok < 50:
+            speed = max(megtfok * (originalspeed*0.02), 10)
         remaining = target-g.angle
         correction = remaining*multiplier
         if correction > 100:
             correction = 100
         elif correction < -100:
             correction = -100
-        if remfok < 200:
-            speed = max(originalspeed*(remfok*0.005), 5)
+        if stop == True:
+            if remfok < 100:
+                speed = max(originalspeed*(remfok*0.01), 10)
             
         ms.on(correction, speed)
         print(remaining, g.angle, (ml.position+mr.position)/2<megt)
@@ -80,6 +81,7 @@ def fordulas_with_timeout(target, multiplier=0.7, timeout=5):
 
     mt.stop()
     time.sleep(0.3)
+    
 def fordulas(target, multiplier=0.7):
     if target < 0:
         target=target*-1
@@ -103,16 +105,16 @@ def mhdt():
     # mozgókép három dimenziónális térben
     #   3d mozi
     g.reset()
-    el(50, 325)
+    el(50, 345)
     kr.on_for_degrees(-30, 450)
     time.sleep(0.2)
-    fordulas_with_timeout(-31, 0.7, 1.75)
+    fordulas_with_timeout(-31, 1.2, 1.75)
     kr.on_for_degrees(30, 600)
     fordulas(10)
     kr.on_for_degrees(-50, 200)
-    hel(-70, -900, 10)
+    hel(-70, -920, 10)
     fordulas(-35)
-    hel(-50, -275, -30)
+    hel(-70, -320  , -30)
     g.reset()
     
     #   jelenetváltás
@@ -120,26 +122,26 @@ def mhdt():
     el(60, 1030, 0)
     kl.on_for_degrees(30, 480)
     time.sleep(0.2)
-    el(50, 230)
+    el(50, 250)
     fordulas(60)
     hel(-10, -175, 60, 1.5)
     time.sleep(0.4)
     fordulas(60)
-    el(45, 73, 60)
+    el(45, 79, 60)
     kr.on_for_degrees(-60, 975)
     el(45, 3, 60)
     time.sleep(1)
-    hel(-50, -60)
+    hel(-60, -60)
     kr.on_for_degrees(40, 375)
     fordulas(-10)
-    el(40, 300, -10, stop=False)
-    el(40, 500, -45)
-    el(40, 400, 0)
+    el(50, 300, -10, stop=False)
+    el(50, 500, -45)
+    el(50, 400, 0)
     g.reset()
     
     #   immerzív tapasztaltat
     time.sleep(0.3)
-    hel(-50, -325, 45)
+    hel(-60, -325, 45)
     fordulas(0)
     kl.on_for_degrees(30, 520)
     time.sleep(0.1)
@@ -153,19 +155,19 @@ def mhdt():
     
     #mozgókamera karjának felnyitása
     fordulas(180)
-    el(30, 350, 220)
+    el(60, 350, 220)
     kl.on_for_degrees(30, 520)
     time.sleep(0.1)
-    hel(-50, -240, 225)
+    hel(-70, -240, 225)
     fordulas(180)
-    el(50, 850, 180)
+    el(60, 880, 180)
     fordulas(224)
-    el(40, 120, 224)
+    el(60, 120, 224)
     time.sleep(0.2)
     kr.on_for_degrees(-30, 1200)
     hel(-30, -500, 180)
     kr.on_for_degrees(30, 600)
-    el(50, 1400, 120)
+    el(100, 1400, 110)
     
 def mk():
     #mozgó kamera ellökése
@@ -176,15 +178,15 @@ def mk():
     hel(-30, -1250, 0)
     time.sleep(2)
     el(60, 1450, 0)
-    hel(-30, -800, 0)
+    hel(-75, -800, 0)
 
 def wb():
     #hangmixer
     g.reset()
-    el(30, 630, 0, 0.5, stop=False)
+    el(40, 630, 0, 0.5, stop=False)
     el(50, 200, 0.5)
     time.sleep(0.1)
-    hel(-20           , -200, -0, 0.8, stop=False)
+    hel(-20, -200, -0, 0.8, stop=False)
     hel(-60, -630)
 
 def szp():
@@ -196,9 +198,9 @@ def szp():
     #színpad
     time.sleep(0.5)
     el(60, 300, -45, multiplier=0.3, stop=False)
-    el(60, 580, -15, 0.3)
+    el(60, 650, -15, 0.3)
     fordulas(-90, 0.5) 
-    el(30, 400, -90, multiplier=0.3,)
+    el(70, 400, -90, multiplier=0.3,)
     kr.on_for_degrees(-30, 300) 
     kl.on_for_degrees(-30, 700)
     hel(-60, -200, -90, 1.5)
@@ -210,21 +212,21 @@ def pft():
     g.reset()
     time.sleep(0.5)
     el(60, 1200, 0, stop=False)
-    el(30, 220, 0,)
+    el(45, 220, 0,)
     kl.on_for_rotations(-30, 3.75)
     #Masterpiece(Tm)
-    hel(-30, -20, 0)
+    hel(-50, -20, 0, 0.5)
     print("hel1")
-    fordulas(74)
+    fordulas(74, 0.6)
     print("fordulas2")
-    el(50, 525, 74)
+    el(60, 425, 74)
     print("el1")
-    el(23, 500, 19)
+    el(30, 575, 19)
     print("el2")
     hel(-30, -300, 74, 0.4)
     #visszamenetel a bázisba
     print("hel2")
-    fordulas(-16, 0.3)
+    fordulas(-16, 0.6)
     print("fordulas3")
     el(60, 1200, -16, stop=False)
     el(60, 500, -76, stop=False)
@@ -235,12 +237,12 @@ def pft():
    
 def csirke():
     g.reset()
-    el(50, 640, 0)
+    el(60, 640, 0)
     fordulas(60)
     fordulas(0, 0.6)
-    el(50, 470)
+    el(70, 470)
     kl.on_for_degrees(100, 4000)
-    hel(-30, -1000)
+    hel(-60, -1000)
 
 valasztas = 0
 balra = 0
@@ -336,14 +338,23 @@ while menu == True:
                 valasztas-=1
                 mongas=False
                 time.sleep(0.2)
+            if btn.down == True:
+                
+                for i in range(0, 4):
+                    kr.on_for_degrees(80, 90, block=False)
+                    kl.on_for_degrees(80, 90, )
+                    kr.on_for_degrees(-80, 90, block=False)
+                    kl.on_for_degrees(-80, 90, )
     except KeyboardInterrupt:
-        mt.off(brake=False)
+        mt.off()
         print("a")
-        ms.off(brake=False)
+        ms.off()
         print("b")
-        kl.off(brake=False)
+        mr.off()
+        ml.off()
+        kl.off()
         print("c")
-        kr.off(brake=False)
+        kr.off()
         print("d")
         pass
     btn = None
